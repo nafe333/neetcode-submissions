a314562e8f15a334d70c09da3ec0bfb0fb424099
Nafea Mostafa <94410012+nafe333@users.noreply.github.com>
2026-06-09 14:34:14 +0300

    Add: lemonade-change - submission-3

---
+++ Data Structures & Algorithms/lemonade-change/submission-3.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func lemonadeChange(_ bills: [Int]) -> Bool {
+
+        var five = 0
+        var ten = 0
+
+for bill in bills {
+if bill == 5 {
+    five += 1
+} else if bill == 10 {
+guard five > 0 else { return false}
+ten += 1
+five -= 1
+} else {
+    if ten > 0 && five > 0 {
+        ten -= 1
+        five -= 1
+    } else if five >= 3{
+        five -= 3
+    } else { return false}
+}
+
+
+
+
+
+}
+return true
+    }
+}
