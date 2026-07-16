f1b3579dc24c749154b6c00012cc702f6c313303
Nafea Mostafa <94410012+nafe333@users.noreply.github.com>
2026-07-16 17:32:02 +0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        var dict: [Character: Int] = [:]
+        for char in s {
+            dict[char, default: 0] += 1
+        }
+        for newChar in t {
+          dict[newChar, default: 0] -= 1
+        }
+        for item in dict {
+            if item.value != 0 {
+                return false
+            }
+        }
+return true
+//return s.sorted() == t.sorted()
+    }
+}
